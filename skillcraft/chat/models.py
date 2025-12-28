6f4d1fad235d776df8a413a457816a23f57daf09
AkhilSK7 <akhilskumar1599@gmail.com>
2025-12-28 21:59:53 +0530

    Revert "Fixed user model for deployment"
    This reverts commit 57458b6f8660b5c6f596d7c28b470d6566d35c73.
    
    e

--- skillcraft/chat/models.py
+++ skillcraft/chat/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth import get_user_model
 
-User = get_user_model()
-
-
+User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class PrivateChatRoom(models.Model):
