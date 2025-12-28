3fc982345682b2e805c3dffde34803024c088e77
AkhilSK7 <akhilskumar1599@gmail.com>
2025-12-28 22:01:43 +0530

    Revert "Added STATIC_ROOT for production"
    This reverts commit 570ae37cdf083bdcd82030ca5adeb6ec90869e44.

--- skillcraft/skillcraft/settings.py
+++ skillcraft/skillcraft/settings.py
@@ -201,8 +201,6 @@ MEDIA_URL='/media/'
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 AUTH_USER_MODEL="users.CustomUser"
 # Default primary key field type
@@ -220,5 +218,4 @@ CHANNEL_LAYERS = {
         },
     },
 }
-
 
